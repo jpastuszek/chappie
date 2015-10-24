b182cc4e4c0e4e0fa5d37875cf4c7333b4bda1f3
Jakub Pastuszek <jpastuszek@gmail.com>
2015-10-25 00:39:03 +0100

    trying to go back to static dispache iterator and enable random test

--- src/search.rs
+++ src/search.rs
@@ -1,7 +1,6 @@
 use std::iter::Iterator;
 use std::collections::HashSet;
 use std::hash::Hash;
-use std::borrow::Borrow;
 
 struct Visited<T> {
     hash_set: HashSet<T>
@@ -26,8 +25,9 @@ impl<T> Visited<T> where T: Hash + Eq {
 pub trait SearchSpace<'a> {
     type State: Hash + Eq;
     type Action;
+    type Iterator: Iterator<Item=(Self::Action, Self::State)>;
 
-    fn expand(&'a self, state: &Self::State) -> Box<Iterator<Item=(Self::Action, Self::State)> + 'a>;
+    fn expand(&'a self, state: &Self::State) -> Self::Iterator;
 
     fn dfs<G>(&'a self, start: Self::State, is_goal: G) -> Option<Vec<Self::Action>>
     where G: Fn(&Self::State) -> bool
@@ -37,7 +37,7 @@ pub trait SearchSpace<'a> {
         }
 
         let mut visited = Visited::new();
-        let mut stack = vec![(self.expand(start.borrow()), None)];
+        let mut stack = vec![(self.expand(&start), None)];
 
         loop {
             let next = match stack.last_mut() {
@@ -56,7 +56,7 @@ pub trait SearchSpace<'a> {
                              .collect()
                     )
                 }
-                stack.push((self.expand(state.borrow()), Some(action)));
+                stack.push((self.expand(&state), Some(action)));
                 visited.insert(state);
             } else {
                 stack.pop();
@@ -76,8 +76,7 @@ pub mod tests {
     use std::iter::Enumerate;
     use std::cell::RefCell;
     use std::collections::HashSet;
-
-    /*
+/*
     struct RandomGraph {
         nodes: Vec<Vec<usize>>
     }
@@ -90,7 +89,7 @@ pub mod tests {
             RandomGraph {
                 nodes: rng_e.gen_iter::<usize>().take(n_nodes)
                     .map(|e| rng_n.gen_iter::<usize>().take(e % max_edges)
-                        .map(|n| n % n_nodes).collect()).collect()
+                    .map(|n| n % n_nodes).collect()).collect()
             }
         }
 
@@ -108,7 +107,7 @@ pub mod tests {
         }
     }
 
-    impl SearchSpace for RandomGraph {
+    impl<'a> SearchSpace<'a> for RandomGraph {
         type State = usize;
         type Action = usize;
         type Iterator = Enumerate<IntoIter<usize>>;
@@ -119,31 +118,6 @@ pub mod tests {
             } else {
                 vec![]
             }.into_iter().enumerate()
-        }
-    }
-
-    struct Observer<T> {
-        goal: T,
-        visited: RefCell<Vec<T>>,
-    }
-
-    impl<T> Observer<T> where T: Clone {
-        pub fn new(goal: T) -> Observer<T> {
-            Observer {
-                goal: goal,
-                visited: RefCell::new(vec![])
-            }
-        }
-
-        pub fn visited(&self) -> Vec<T> {
-            self.visited.borrow().clone()
-        }
-    }
-
-    impl<T> SearchGoal<T> for Observer<T> where T: PartialEq + Clone {
-        fn is_goal(&self, state: &T) -> bool {
-            self.visited.borrow_mut().push(state.clone());
-            self.goal == *state
         }
     }
     */
@@ -158,16 +132,15 @@ pub mod tests {
         impl<'a> SearchSpace<'a> for TestSearch {
             type State = i32;
             type Action = Dir;
+            type Iterator = IntoIter<(Self::Action, Self::State)>;
 
-            fn expand(&'a self, state: &Self::State) -> Box<Iterator<Item=(Self::Action, Self::State)> + 'a> {
-                Box::new(
-                    match *state {
-                        0 => vec![(Dir::Left, 1), (Dir::Right, 2)],
-                        1 => vec![(Dir::Left, 3), (Dir::Right, 4)],
-                        2 => vec![(Dir::Left, 2)],
-                        _ => vec![]
-                    }.into_iter()
-                )
+            fn expand(&'a self, state: &Self::State) -> Self::Iterator {
+                match *state {
+                    0 => vec![(Dir::Left, 1), (Dir::Right, 2)],
+                    1 => vec![(Dir::Left, 3), (Dir::Right, 4)],
+                    2 => vec![(Dir::Left, 2)],
+                    _ => vec![]
+                }.into_iter()
             }
         }
 
@@ -194,12 +167,11 @@ pub mod tests {
         impl<'a> SearchSpace<'a> for TestSearch {
             type State = &'a u32;
             type Action = &'a Dir;
+            type Iterator = IntoIter<(Self::Action, Self::State)>;
 
-            fn expand(&'a self, state: &Self::State) -> Box<Iterator<Item=(Self::Action, Self::State)> + 'a> {
-                Box::new(
-                    self.nodes.iter().nth(**state as usize).expect(format!("no state: {}", *state).trim()).iter()
-                    .map(|&(ref a, ref s)| (a, s))
-                )
+            fn expand(&'a self, state: &Self::State) -> Self::Iterator {
+                self.nodes.iter().nth(**state as usize).expect(format!("no state: {}", *state).trim()).iter()
+                .map(|&(ref a, ref s)| (a, s)).collect::<Vec<_>>().into_iter()
             }
         }
 
@@ -222,7 +194,6 @@ pub mod tests {
         assert!(ts.dfs(&2, |&x| *x == 0).is_none());
     }
 
-/*
     #[test]
     pub fn test_dfs_random() {
         const N_NODES: usize = 48;
@@ -230,19 +201,19 @@ pub mod tests {
 
         let g = RandomGraph::new(N_NODES, MAX_EDGES);
 
-        assert!(g.dfs(&N_NODES, &0).is_none());
-        assert!(g.dfs(&0, &N_NODES).is_none());
+        assert!(g.dfs(N_NODES, |&g| g == 0).is_none());
+        assert!(g.dfs(0, |&g| g == N_NODES).is_none());
 
         for start in (0..N_NODES) {
             for goal in (0..N_NODES) {
-                let observer = Observer::new(goal);
-                if let Some(path) = g.dfs(&start, &observer) {
+                if let Some(path) = g.dfs(start, |&g| g == goal) {
                     let mut state = start;
                     for action in path {
                         state = g.expand(&state).skip(action).next().unwrap().1;
                     }
                     assert_eq!(state, goal);
                 } else {
+                    /*
                     let visited: HashSet<_> = observer.visited().iter().cloned().collect();
                     for state in observer.visited() {
                         assert!(!observer.is_goal(&state));
@@ -250,9 +221,9 @@ pub mod tests {
                             assert!(visited.contains(&next_state));
                         }
                     }
+                    */
                 }
             }
         }
     }
-*/
 }
